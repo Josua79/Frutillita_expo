4d8ef164fb9d5d2abf95939ac70246696049e59e
Freddy <fegamon1999@gmail.com>
2022-09-02 14:53:21 -0500

    first commit

---
+++ src/constants/colors.js
@@ -0,0 +1,22 @@
+export default {
+  black: '#000000',
+  black20: 'rgba(0, 0, 0, 0.2)',
+  black40: 'rgba(0, 0, 0, 0.4)',
+  black50: 'rgba(0, 0, 0, 0.5)',
+  black70: 'rgba(0, 0, 0, 0.7)',
+  white: '#ffffff',
+  blackBlur: '#161616',
+
+  // theme colors
+  blackBg: '#4E001D',
+  transparent: 'transparent',
+  brandPrimary: '#57b660',
+  grey: '#282828',
+  greyInactive: '#b3b3b3',
+  greyLight: '#bebebe',
+  greySwitchBorder: '#404040',
+  greyOff: '#3e3e3e',
+  grey3: '#333',
+  bgTab: '#014800',
+  bgTabInactive: '#3C0016',
+};
---
+++ src/screens/Home.js
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { colors, device, gStyle } from '../constants';
+
+// components
+import AlbumsHorizontal from '../components/AlbumsHorizontal';
+
+// mock data
+import heavyRotation from '../mockdata/heavyRotation.json';
+import jumpBackIn from '../mockdata/jumpBackIn.json';
+import recentlyPlayed from '../mockdata/recentlyPlayed.json';
+
+const Home = () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  const opacityIn = scrollY.interpolate({
+    inputRange: [0, 128],
+    outputRange: [0, 1],
+    extrapolate: 'clamp'
+  });
+
+  const opacityOut = scrollY.interpolate({
+    inputRange: [0, 88],
+    outputRange: [1, 0],
+    extrapolate: 'clamp'
+  });
+
+  return (
+    <React.Fragment>      
+      <Animated.ScrollView
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        scrollEventThrottle={16}
+        showsVerticalScrollIndicator={false}
+        style={gStyle.container}
+      >
+        <View style={{margin:25}} />
+
+        <AlbumsHorizontal data={recentlyPlayed} heading="Ultimas reproducciones" />
+
+        <AlbumsHorizontal
+          data={heavyRotation}
+          heading="Tendencias"
+          tagline="Se reproduce como pan caliente."
+        />
+
+        <AlbumsHorizontal
+          data={jumpBackIn}
+          heading="Recomendados para tí"
+          tagline="Músicas para tí según basadas en tus gustos."
+        />
+      </Animated.ScrollView>
+    </React.Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  iPhoneNotch: {
+    backgroundColor: colors.black70,
+    height: 44,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    zIndex: 20
+  },
+  containerHeader: {
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 16,
+    paddingTop: device.iPhoneNotch ? 60 : 36,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    zIndex: 10
+  }
+});
+
+export default Home;
